ce62e53ae3fd794e7e3b52699c03074828950558
patrick tully <patrick@avatarnewyork.com>
2020-01-31 17:36:52 -0500

    supress stdout

--- xunit2junit.sh
+++ xunit2junit.sh
@@ -2,5 +2,5 @@
 
 stdin=$(cat)
 echo $stdin > /xunit.xml
-/root/.dotnet/tools/dotnet-xunit-to-junit /xunit.xml /junit.xml
+/root/.dotnet/tools/dotnet-xunit-to-junit /xunit.xml /junit.xml &>/dev/null
 cat /junit.xml
